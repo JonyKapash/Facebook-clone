1e7b87611524312497a0e9435e8ca1d1fc9e0552
Yonatan Kapash <jonykapash@gmail.com>
2021-02-16 10:51:17 +0200

    Added header component +header Icons and

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
 	return (
 		//BEM naming convention
 		<div className="app">
-			<h1>Lets build facebook!</h1>
+			<Header />
+
+			{/* App body */}
+			{/* Sidebar */}
+			{/* Feed */}
+			{/* Widgets */}
 		</div>
 	);
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyD_gkEaKWKUyFiF2kYzMQuDST42gQFGiHE",
+	authDomain: "facebook-clone-de286.firebaseapp.com",
+	projectId: "facebook-clone-de286",
+	storageBucket: "facebook-clone-de286.appspot.com",
+	messagingSenderId: "857026212822",
+	appId: "1:857026212822:web:10d1e01cf575ea36eb6a77",
+};
