6cc13ee20526cf1370a354c80c682fd3d6598bfc
Yonatan Kapash <jonykapash@gmail.com>
2021-02-17 18:07:34 +0200

    Updating MessageSender , adding button + Icons

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./Feed.css";
+import MessageSender from "./MessageSender";
 import StoryReel from "./StoryReel";
 function Feed() {
 	return (
 		<div className="feed">
 			<StoryReel />
-			{/* MessageSender */}
+			<MessageSender />
 		</div>
 	);
 }
