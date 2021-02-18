4b225347420a25fd8d6dd1c882bcde24e8c515b1
Yonatan Kapash <jonykapash@gmail.com>
2021-02-18 15:50:58 +0200

    Added Widgets plugin

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Feed from "./Feed";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Widgets from "./Widgets";
 
 function App() {
 	return (
@@ -13,7 +14,7 @@ function App() {
 			<div className="app__body">
 				<Sidebar />
 				<Feed />
-				{/* Widgets */}
+				<Widgets />
 			</div>
 			
 		</div>
